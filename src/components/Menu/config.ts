196afdad966c88be8e112bcdf9d43f0518effc80
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2022-08-15 10:19:48 -0400

    omg

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,7 +4,7 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'dogefarm.bourbondefi.com/',
+    href: 'https://dogefarm.bourbondefi.com/',
   },
   {
     label: 'Trade',
